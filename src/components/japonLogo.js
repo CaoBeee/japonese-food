a0247c4b2dfdad2c30590af95c67cafe855715b0
Brian Cao <98575161+CaoBeee@users.noreply.github.com>
2023-05-19 16:57:48 -0700

    Fixed hydration error

--- src/components/japonLogo.js
+++ src/components/japonLogo.js
@@ -1,26 +1,30 @@
+import React from 'react'
 import styles from '../styles/japonLogo.module.css'
-// import Layout from './layout'
 import Image from 'next/image'
-import logo from '../../public/img/logo_japonesefood.png'
+import logoSVG from '../../public/img/logo_japonesefood.png'
 import logoVector from '/public/img/LogoVector.svg'
 
 export default function JaponLogo() {
 	return (
-		<div className={styles.logoContainer}>
+		<React.Fragment>
 			<Image
 				className={styles.vector}
 				src={logoVector}
-				alt='shape'
+				alt=''
 				height={100}
 				width={200}
+				priority
 			/>
-			<Image
-				className={styles.logo}
-				src={logo}
-				alt='Japon Logo'
-				height={100}
-				width={100}
-			/>
-		</div>
+			<a href='/'>
+				<Image
+					className={styles.logo}
+					src={logoSVG}
+					alt=''
+					height={100}
+					width={100}
+					priority
+				/>
+			</a>
+		</React.Fragment>
 	)
 }
