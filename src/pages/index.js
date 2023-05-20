972d7c2440c4543ab4c7a5ae2970df0408a07676
Spencer-Breding <Spencerbreding@gmail.com>
2023-05-20 02:09:28 -0700

    Revert "Moved Home page from app folder to src/pages"
    This reverts commit f6a1a4afef20e023078f41b46573923f26e7a62d.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,1 @@
-//App Level Page
-import React from 'react'
-import favicon from '../../public/favicon.ico'
-import Layout from '../components/layout'
-
-export default function Home() {
-	return (
-		<React.Fragment>
-			<Layout/>
-		</React.Fragment>
-	)
-}
+// JavaScript source code
