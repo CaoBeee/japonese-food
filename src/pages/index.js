f6a1a4afef20e023078f41b46573923f26e7a62d
Spencer-Breding <Spencerbreding@gmail.com>
2023-05-20 00:07:20 -0700

    Moved Home page from app folder to src/pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,12 @@
-// JavaScript source code
+//App Level Page
+import React from 'react'
+import favicon from '../../public/favicon.ico'
+import Layout from '../components/layout'
+
+export default function Home() {
+	return (
+		<React.Fragment>
+			<Layout/>
+		</React.Fragment>
+	)
+}
