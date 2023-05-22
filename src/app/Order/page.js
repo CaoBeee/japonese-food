4697a2ed6dc042e3c4d7757dabef917c312080be
Spencer-Breding <Spencerbreding@gmail.com>
2023-05-22 06:38:06 -0700

    Finished Menu Item Component

--- src/app/Order/page.js
+++ src/app/Order/page.js
@@ -1,15 +1,30 @@
 "use client";
-import React, { useState } from 'react';
+import React, {useEffect, useState } from 'react';
 import MenuSelections from '../../components/menuSelections';
 import PopularGallery from '../../components/popularGallery';
+import SushiRollGallery from '../../components/sushiRollGallery';
+import { getCookie} from 'cookies-next';
 import styles from '../../styles/order.module.css';
 
 export default function Order(props) {
-
+    const [selectionState, setSelectionState] = useState(0);
+    useEffect(() => {
+        setSelectionState(getCookie('menu_state'));
+        const listenCookieChange = () => {
+            setSelectionState(getCookie('menu_state'));
+        }
+        window.addEventListener("cookie", listenCookieChange);
+        return () => window.removeEventListener("cookie", listenCookieChange);
+    }, [])
     return (
         <div className={styles.container }>
             <MenuSelections />
-            <PopularGallery />
+            {selectionState == 1 &&
+                <PopularGallery />
+            }
+            {selectionState == 2 &&
+                <SushiRollGallery />
+            }
         </div>
     )
 }
--- src/components/menuSelections.js
+++ src/components/menuSelections.js
@@ -24,16 +24,16 @@ export default function MenuSelections() {
         <span className={styles.container}>
             <MenuText color={selection == 1 && '#ED5B55' || 'black'} fontSize={selection == 1 && 32 || 30} onClick={() => { setSelection(1), setCookie('menu_state', 1); window.dispatchEvent(new Event("cookie")) }}
                 onMouseEnter={() => setCurHovered(1)} onMouseLeave={() => setCurHovered(0)} textDecoration={curHovered == 1 && selection != 1 && hoveredDecoration || notHoveredDecoration}
-                textShadow={selection == 1 && "2px 0 4px #ed5b55" || "none"} title='Popular Items' />
+                textShadow={selection == 1 && "2px 0 4px rgba(237, 91, 85, 0.5)" || "none"} title='Popular Items' />
             <MenuText color={selection == 2 && '#ED5B55' || 'black'} fontSize={selection == 2 && 32 || 30} onClick={() => { setSelection(2), setCookie('menu_state', 2); window.dispatchEvent(new Event("cookie")) }}
                 onMouseEnter={() => setCurHovered(2)} onMouseLeave={() => setCurHovered(0)} textDecoration={curHovered == 2 && selection != 2 && hoveredDecoration || notHoveredDecoration}
-                textShadow={selection == 2 && "2px 0 4px #ed5b55" || "none"}  title='Sushi Rolls' />
+                textShadow={selection == 2 && "2px 0 4px rgba(237, 91, 85, 0.5)" || "none"}  title='Sushi Rolls' />
             <MenuText color={selection == 3 && '#ED5B55' || 'black'} fontSize={selection == 3 && 32 || 30} onClick={() => { setSelection(3), setCookie('menu_state', 3); window.dispatchEvent(new Event("cookie")) }}
                 onMouseEnter={() => setCurHovered(3)} onMouseLeave={() => setCurHovered(0)} textDecoration={curHovered == 3 && selection != 3 && hoveredDecoration || notHoveredDecoration}
-                textShadow={selection == 3 && "2px 0 4px #ed5b55" || "none"}  title='Sashimi & Nigiri' />
+                textShadow={selection == 3 && "2px 0 4px rgba(237, 91, 85, 0.5)" || "none"}  title='Sashimi & Nigiri' />
             <MenuText color={selection == 4 && '#ED5B55' || 'black'} fontSize={selection == 4 && 32 || 30} onClick={() => { setSelection(4), setCookie('menu_state', 4); window.dispatchEvent(new Event("cookie")) }}
                 onMouseEnter={() => setCurHovered(4)} onMouseLeave={() => setCurHovered(0)} textDecoration={curHovered == 4 && selection != 4 && hoveredDecoration || notHoveredDecoration}
-                textShadow={selection == 4 && "2px 0 4px #ed5b55" || "none" } title='Sides & Beverages' />
+                textShadow={selection == 4 && "2px 0 4px rgba(237, 91, 85, 0.5)" || "none" } title='Sides & Beverages' />
         </span>
     )
 }
