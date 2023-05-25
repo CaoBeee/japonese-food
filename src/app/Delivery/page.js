b9ebb3b70aafdf9450ef7e0aed7fb1a652d58d0f
Brian Cao <98575161+CaoBeee@users.noreply.github.com>
2023-05-24 18:05:59 -0700

    Optimized navbar to apply new nextjs router features

--- src/app/Delivery/page.js
+++ src/app/Delivery/page.js
@@ -10,30 +10,32 @@ export default function Delivery() {
 		<React.Fragment>
 			<div className={styles.container}>
 				<div className={styles.headerContainer}>
-					<div className={styles.header}>
-						<h1>
-							You order,
-							<br />
-							<span className={styles.headerSpan}> we deliver.</span>
-							<br />
-						</h1>
-						<div className={styles.addressBtn}>
-							<Image
-								className={styles.addressIcon}
-								src={addressIcon}
-								alt='Icon'
-							/>
-							<input
-								placeholder='Enter delivery address'
-								type='text'
-							/>
-							<Image
-								src={arrow}
-								alt='Icon'
-							/>
+					<div className={styles.leftContainer}>
+						<div className={styles.header}>
+							<h1>
+								You order,
+								<br />
+								<span className={styles.headerSpan}> we deliver.</span>
+								<br />
+							</h1>
+							<div className={styles.addressBtn}>
+								<Image
+									className={styles.addressIcon}
+									src={addressIcon}
+									alt='Icon'
+								/>
+								<input
+									placeholder='Enter delivery address'
+									type='text'
+								/>
+								<Image
+									src={arrow}
+									alt='Icon'
+								/>
+							</div>
+							<p>Not ready to order yet? Take a look at our menu for something delicious.</p>
+							<button className={styles.menuBtn}>Menu</button>
 						</div>
-						<p>Not ready to order yet? Take a look at our menu for something delicious.</p>
-						<button className={styles.menuBtn}>Menu</button>
 					</div>
 				</div>
 				<div
