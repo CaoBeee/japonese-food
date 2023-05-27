3713863c7385ccdf4f03c0aad0f64c2f4616345b
Brian Cao <98575161+CaoBeee@users.noreply.github.com>
2023-05-27 15:28:44 -0700

    Sushi background image optimization and componentized autocomplete input

--- src/app/Delivery/page.js
+++ src/app/Delivery/page.js
@@ -1,17 +1,11 @@
 'use client'
-
 import React from 'react'
 import Image from 'next/image'
 import styles from '@/styles/Delivery.module.css'
 import sushi from '/public/img/delivery/Sushi.svg'
-import addressIcon from '/public/img/delivery/InputAddressIcon.svg'
-import arrow from '/public/img/delivery/InputArrowIcon.svg'
-import {
-	GeoapifyGeocoderAutocomplete,
-	GeoapifyContext,
-} from '@geoapify/react-geocoder-autocomplete'
-import '@geoapify/geocoder-autocomplete/styles/minimal.css'
 import Link from 'next/link'
+import AutocompleteContext from '@/components/autocompleteContext'
+import Head from 'next/head'
 
 export default function Delivery() {
 	//GEOAPIFY
@@ -24,66 +18,47 @@ export default function Delivery() {
 	}
 
 	return (
-		<div className={styles.container}>
-			<div className={styles.headerContainer}>
-				<div className={styles.leftContainer}>
-					<div style={{ display: 'block' }}>
-						<div className={styles.header}>
-							<h1>
-								You order,
-								<br />
-								<span className={styles.headerSpan}> we deliver.</span>
-								<br />
-							</h1>
-						</div>
-						<GeoapifyContext apiKey={process.env.NEXT_PUBLIC_GEOAPIFY_API_KEY}>
-							<div
-								id='autocomplete'
-								className='autocomplete-container'
-							>
-								<Image
-									className={styles.addressIcon}
-									src={addressIcon}
-									alt='Icon'
-								/>
-								<GeoapifyGeocoderAutocomplete
-									limit={2}
-									placeholder='Enter delivery address'
-									BiasByCountryCode={'US'}
-									skipIcons={false}
-									debounceDelay={300}
-									placeSelect={onPlaceSelect}
-									suggestionsChange={onSuggestionChange}
-								></GeoapifyGeocoderAutocomplete>
-								<Link
-									className={styles.arrow}
-									href={'/Order'}
-								>
-									<Image
-										src={arrow}
-										alt='Icon'
-									/>
-								</Link>
+		<>
+			<Head>
+				<link
+					rel='preload'
+					href='/public/img/delivery/Sushi.svg'
+					as='image'
+				/>
+			</Head>
+			<div className={styles.container}>
+				<div className={styles.headerContainer}>
+					<div className={styles.leftContainer}>
+						<div style={{ display: 'block' }}>
+							<div className={styles.header}>
+								<h1>
+									You order,
+									<br />
+									<span className={styles.headerSpan}> we deliver.</span>
+									<br />
+								</h1>
 							</div>
-						</GeoapifyContext>
-						<p>Not ready to order yet? Take a look at our menu for something delicious.</p>
-						<Link
-							className={styles.menuBtnLink}
-							href={'/Order'}
-						>
-							<button className={styles.menuBtn}>Menu</button>
-						</Link>
+							<AutocompleteContext></AutocompleteContext>
+							<p>Not ready to order yet? Take a look at our menu for something delicious.</p>
+							<Link
+								className={styles.menuBtnLink}
+								href={'/Order'}
+							>
+								<button className={styles.menuBtn}>Menu</button>
+							</Link>
+						</div>
 					</div>
 				</div>
+				<div className={styles.sushiBackgroundContainer}>
+					<Image
+						src={sushi.src}
+						alt='sushi'
+						fill='fill'
+						className={styles.sushi}
+						priority={true}
+					></Image>
+				</div>
 			</div>
-			<div
-				className={styles.sushi}
-				style={{
-					backgroundImage: `url(${sushi.src})`,
-					backgroundRepeat: 'no-repeat',
-					backgroundSize: '900px 810px',
-				}}
-			></div>
-		</div>
+		</>
 	)
 }
--- src/app/layout.js
+++ src/app/layout.js
@@ -29,7 +29,7 @@ export default function RootLayout({ children }) {
 				<div className='lineContainer'>
 					<Image
 						src={topVectorLine}
-						alt=''
+						alt='sushi'
 						className='topVectorLine'
 					/>
 				</div>{' '}
