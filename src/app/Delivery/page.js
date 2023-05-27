880eb94632ecb45513da88a4ad0acc40781b1c23
Brian Cao <98575161+CaoBeee@users.noreply.github.com>
2023-05-26 20:40:35 -0700

    Address auto complete functionality

--- src/app/Delivery/page.js
+++ src/app/Delivery/page.js
@@ -1,37 +1,71 @@
-import React from 'react'
+'use client'
+
+import React, { useEffect } from 'react'
 import Image from 'next/image'
 import styles from '@/styles/Delivery.module.css'
 import sushi from '/public/img/delivery/Sushi.svg'
 import addressIcon from '/public/img/delivery/InputAddressIcon.svg'
 import arrow from '/public/img/delivery/InputArrowIcon.svg'
+import {
+	GeoapifyGeocoderAutocomplete,
+	GeoapifyContext,
+} from '@geoapify/react-geocoder-autocomplete'
+import '@geoapify/geocoder-autocomplete/styles/minimal.css'
+import Link from 'next/link'
 
 export default function Delivery() {
+	//GEOAPIFY
+	function onPlaceSelect(value) {
+		console.log(value)
+	}
+
+	function onSuggestionChange(value) {
+		console.log(value)
+	}
+
 	return (
 		<div className={styles.container}>
 			<div className={styles.headerContainer}>
 				<div className={styles.leftContainer}>
-					<div className={styles.header}>
-						<h1>
-							You order,
-							<br />
-							<span className={styles.headerSpan}> we deliver.</span>
-							<br />
-						</h1>
-						<div className={styles.addressBtn}>
-							<Image
-								className={styles.addressIcon}
-								src={addressIcon}
-								alt='Icon'
-							/>
-							<input
-								placeholder='Enter delivery address'
-								type='text'
-							/>
-							<Image
-								src={arrow}
-								alt='Icon'
-							/>
+					<div style={{ display: 'block' }}>
+						<div className={styles.header}>
+							<h1>
+								You order,
+								<br />
+								<span className={styles.headerSpan}> we deliver.</span>
+								<br />
+							</h1>
 						</div>
+						<GeoapifyContext apiKey='c87f9e92f29a46c49a3fed25f7358d31'>
+							<div
+								id='autocomplete'
+								className='autocomplete-container'
+							>
+								<Image
+									className={styles.addressIcon}
+									src={addressIcon}
+									alt='Icon'
+								/>
+								<GeoapifyGeocoderAutocomplete
+									limit={2}
+									placeholder='Enter delivery address'
+									BiasByCountryCode={'US'}
+									skipIcons={false}
+									debounceDelay={300}
+									placeSelect={onPlaceSelect}
+									suggestionsChange={onSuggestionChange}
+								></GeoapifyGeocoderAutocomplete>
+								<Link
+									className={styles.arrow}
+									href={'/Order'}
+								>
+									<Image
+										src={arrow}
+										alt='Icon'
+									/>
+								</Link>
+							</div>
+						</GeoapifyContext>
 						<p>Not ready to order yet? Take a look at our menu for something delicious.</p>
 						<button className={styles.menuBtn}>Menu</button>
 					</div>
