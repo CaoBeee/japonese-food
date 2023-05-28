2307244b143090c3a6a86be6d765f43f3c612acc
Brian Cao <98575161+CaoBeee@users.noreply.github.com>
2023-05-27 20:16:11 -0700

    Switched out sushi background image for a smaller sized image for faster loading

--- src/app/Delivery/page.js
+++ src/app/Delivery/page.js
@@ -2,10 +2,9 @@
 import React from 'react'
 import Image from 'next/image'
 import styles from '@/styles/Delivery.module.css'
-import sushi from '/public/img/delivery/Sushi.svg'
+import sushi from '/public/img/delivery/Sushi.png'
 import Link from 'next/link'
 import AutocompleteContext from '@/components/autocompleteContext'
-import Head from 'next/head'
 
 export default function Delivery() {
 	//GEOAPIFY
@@ -18,47 +17,40 @@ export default function Delivery() {
 	}
 
 	return (
-		<>
-			<Head>
-				<link
-					rel='preload'
-					href='/public/img/delivery/Sushi.svg'
-					as='image'
-				/>
-			</Head>
-			<div className={styles.container}>
-				<div className={styles.headerContainer}>
-					<div className={styles.leftContainer}>
-						<div style={{ display: 'block' }}>
-							<div className={styles.header}>
-								<h1>
-									You order,
-									<br />
-									<span className={styles.headerSpan}> we deliver.</span>
-									<br />
-								</h1>
-							</div>
-							<AutocompleteContext></AutocompleteContext>
-							<p>Not ready to order yet? Take a look at our menu for something delicious.</p>
-							<Link
-								className={styles.menuBtnLink}
-								href={'/Order'}
-							>
-								<button className={styles.menuBtn}>Menu</button>
-							</Link>
+		<div className={styles.container}>
+			<div className={styles.headerContainer}>
+				<div className={styles.leftContainer}>
+					<div style={{ display: 'block' }}>
+						<div className={styles.header}>
+							<h1>
+								You order,
+								<br />
+								<span className={styles.headerSpan}> we deliver.</span>
+								<br />
+							</h1>
 						</div>
+						<AutocompleteContext></AutocompleteContext>
+						<p>Not ready to order yet? Take a look at our menu for something delicious.</p>
+						<Link
+							className={styles.menuBtnLink}
+							href={'/Order'}
+						>
+							<button className={styles.menuBtn}>Menu</button>
+						</Link>
 					</div>
 				</div>
-				<div className={styles.sushiBackgroundContainer}>
-					<Image
-						src={sushi.src}
-						alt='sushi'
-						fill='fill'
-						className={styles.sushi}
-						priority={true}
-					></Image>
-				</div>
 			</div>
-		</>
+
+			<div className={styles.sushiBackgroundContainer}>
+				<Image
+					src={sushi.src}
+					alt='sushi'
+					width={822}
+					height={859}
+					className={styles.sushi}
+					priority={true}
+				></Image>
+			</div>
+		</div>
 	)
 }
