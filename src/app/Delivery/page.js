abc8661f5fa1c35da5c50d0bf4542a31e69156cd
Brian Cao <98575161+CaoBeee@users.noreply.github.com>
2024-05-27 18:40:43 -0700

    Removed other metadata usage

--- src/app/Delivery/page.js
+++ src/app/Delivery/page.js
@@ -6,43 +6,44 @@ import sushi from '/public/img/delivery/Sushi.png'
 import Link from 'next/link'
 import AutocompleteContext from '@/components/autocompleteContext'
 
+/* Causes build failure due to 'use client with metadata'
 export const metadata = {
 	title: 'Delivery',
 	description: "Delivery Page"
 }
+*/
 
 export default function Delivery() {
-
 	return (
 		<>
 			<title>Delivery</title>
-		<div className={styles.container}>
-			<div className={styles.leftContainer}>
-				<h1 className={styles.header}> You order,</h1>
-				<span className={styles.headerSpan}> we deliver.</span>
-				<AutocompleteContext />
-				<p className={styles.paragraph}>
-					Not ready to order yet? Take a look at our menu for something delicious.
-				</p>
-				<Link
-					className={styles.menuBtnLink}
-					href={'/Order'}
-				>
-					<button className={styles.menuBtn}>Menu</button>
-				</Link>
-			</div>
+			<div className={styles.container}>
+				<div className={styles.leftContainer}>
+					<h1 className={styles.header}> You order,</h1>
+					<span className={styles.headerSpan}> we deliver.</span>
+					<AutocompleteContext />
+					<p className={styles.paragraph}>
+						Not ready to order yet? Take a look at our menu for something delicious.
+					</p>
+					<Link
+						className={styles.menuBtnLink}
+						href={'/Order'}
+					>
+						<button className={styles.menuBtn}>Menu</button>
+					</Link>
+				</div>
 
-			<div className={styles.sushiBackgroundContainer}>
-				<Image
-					src={sushi.src}
-					alt='sushi'
-					width={908}
-					height={951}
-					className={styles.sushi}
-					priority={true}
-				/>
+				<div className={styles.sushiBackgroundContainer}>
+					<Image
+						src={sushi.src}
+						alt='sushi'
+						width={908}
+						height={951}
+						className={styles.sushi}
+						priority={true}
+					/>
+				</div>
 			</div>
-			</div>
-			</>
+		</>
 	)
 }
