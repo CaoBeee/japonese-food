a1c84a43e20a88ac988561dd7319f0a7e3f06a83
Brian Cao <98575161+CaoBeee@users.noreply.github.com>
2024-05-27 18:58:56 -0700

    local storage reference error fix

--- src/app/Order/page.js
+++ src/app/Order/page.js
@@ -10,34 +10,36 @@ import { usePathname } from 'next/navigation'
 
 export default function Order() {
 	const { cartItems } = useContext(CartContext)
-	const deliveryAddress = localStorage.getItem('deliveryAddress')
+	if (typeof window !== 'undefined') {
+		const deliveryAddress = localStorage.getItem('deliveryAddress')
+	}
 	return (
 		<>
 			<title>Order</title>
-		<div className={styles.container}>
-			<MenuSelections />
-			<MenuGallery />
-			<div className={styles.order_container}>
-				<Cart cartItems={cartItems} />
-				<div className={styles.address}>
-					<p>
-						<b>
-							<em>Delivering to:</em>
-						</b>
-						<br />
-						{deliveryAddress}
-						<br />
-						<br />
-						<Link
-							className={usePathname() === '/Delivery' ? styles.active : ''}
-							href='/Delivery'
-						>
-							Click here to change
-						</Link>
-					</p>
+			<div className={styles.container}>
+				<MenuSelections />
+				<MenuGallery />
+				<div className={styles.order_container}>
+					<Cart cartItems={cartItems} />
+					<div className={styles.address}>
+						<p>
+							<b>
+								<em>Delivering to:</em>
+							</b>
+							<br />
+							{deliveryAddress}
+							<br />
+							<br />
+							<Link
+								className={usePathname() === '/Delivery' ? styles.active : ''}
+								href='/Delivery'
+							>
+								Click here to change
+							</Link>
+						</p>
+					</div>
 				</div>
 			</div>
-		</div>
 		</>
 	)
 }
